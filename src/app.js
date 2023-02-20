e0d7790671481a11d4ab48f79535e85ef3e2a707
PrinceM <parames.pasitprachaja@gmail.com>
2023-02-20 16:38:12 +0700

    feat(chat): can initial chat (need to set env for proper client url

--- src/app.js
+++ src/app.js
@@ -2,7 +2,21 @@
 // sequelize.sync({ force: true });
 
 require("dotenv").config();
+
+// setting cors for socket.io
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST"]
+};
+
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, { cors: corsOptions });
+
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
@@ -15,8 +29,6 @@ const authRoute = require("./routes/auth-route");
 const notFoundMiddleWare = require("./middlewares/not-found");
 const errorMiddleWare = require("./middlewares/error");
 
-const app = express();
-
 // middleware
 app.use(cors()); // to connect with front-end through web browser
 app.use(helmet()); // to protect http
@@ -26,12 +38,26 @@ app.use(express.json()); // to get BODY data
 // router
 app.use("/auth", authRoute);
 
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+
+  socket.on("message", (data) => {
+    console.log(`Received message: ${data}`);
+    io.emit("message", data);
+  });
+});
+
 // middleware error
 app.use(notFoundMiddleWare);
 app.use(errorMiddleWare);
 
 // start port
 const port = process.env.PORT || 8000;
-app.listen(port, () =>
+server.listen(port, () =>
   console.log(chalk.yellowBright.italic.bold(`Start server at port ${port}...`))
 );
