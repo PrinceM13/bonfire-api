a8e7e630ae2da29910ed4763bfd9036f63255f51
ToonThanwaDev <toon.thanwa@hotmail.com>
2023-02-21 13:16:19 +0700

    hotfix: change app to router in route

--- src/routes/event-route.js
+++ src/routes/event-route.js
@@ -4,13 +4,13 @@ const eventController = require("../controllers/event-controller");
 
 const router = express.Router();
 
-app.post("/", eventController.createEvent);
-app.get("/", eventController.getAllEvents);
-// app.get("/?category=category")
-// app.get("/?date=date")
-// app.get("/?location=location")
-app.get("/:eventId", eventController.getEventsById);
-app.patch("/:eventId", eventController.updateEvents);
-app.delete("/:eventId", eventController.deleteEvents);
+router.post("/", eventController.createEvent);
+router.get("/", eventController.getAllEvents);
+// router.get("/?category=category")
+// router.get("/?date=date")
+// router.get("/?location=location")
+router.get("/:eventId", eventController.getEventsById);
+router.patch("/:eventId", eventController.updateEvents);
+router.delete("/:eventId", eventController.deleteEvents);
 
 module.exports = router;
