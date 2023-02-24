5c9b5e5d806c455e68803214fed8dfd7bad8c8a9
purinutss <purinuts2541@gmail.com>
2023-02-24 19:43:47 +0700

    validate-email-realtime

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -2,10 +2,12 @@ require("../config/passport-jwt");
 
 const express = require("express");
 
+const { User } = require("../models");
 const googleLoginMiddleWare = require("../middlewares/google-login");
 const googleRegisterMiddleWare = require("../middlewares/google-register");
 const authController = require("../controllers/auth-controller");
 const passportAuthenticate = require("../middlewares/passport-authen");
+const crudController = require("../controllers/crud-controller");
 
 const router = express.Router();
 
@@ -16,5 +18,7 @@ router.post("/register-with-google", googleRegisterMiddleWare, authController.lo
 router.get("/me", passportAuthenticate, (req, res) => {
   res.send(req.user);
 });
+
+router.get("/check-email", crudController.getAllRecords(User, "email"));
 
 module.exports = router;
