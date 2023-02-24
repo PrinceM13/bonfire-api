eb9da5a6d548fd84def29dfba65c15e232938a8b
PrinceM <parames.pasitprachaja@gmail.com>
2023-02-24 17:10:12 +0700

    feat(chat-room): chat-room by eventId

--- src/app.js
+++ src/app.js
@@ -44,14 +44,52 @@ io.on("connection", (socket) => {
   console.log("a user connected");
   console.log(socket.id);
 
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
+  // client subscribe to specific room
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    socket.to(room).emit("message", {
+      user: "connect",
+      content: `<--- user: ${String(socket.id).slice(1, 5)} is connected`
+    });
   });
 
+  // // broadcast to all clients (except incoming client) -------------------------------
+  // socket.broadcast.emit("message", {
+  //   user: "connect",
+  //   content: `<--- user: ${String(socket.id).slice(1, 5)} is connected`
+  // });
+
+  // // send incoming message to all clients
+  // socket.on("message", (data) => {
+  //   console.log(`Received message: ${data}`);
+  //   io.emit("message", { user: socket.id, content: data });
+  // });
+  // ------------------------------------------------------------------------------------
+
+  // send incoming message to all clients in specific room
   socket.on("message", (data) => {
     console.log(`Received message: ${data}`);
-    io.emit("message", data);
+    io.to(data.eventId).emit("message", data);
   });
+
+  // client leave from specific room
+  socket.on("leaveRoom", (room) => {
+    socket.to(room).emit("message", {
+      user: "disconnect",
+      content: `user: ${String(socket.id).slice(1, 5)} is disconnected --->`
+    });
+    socket.leave(room);
+  });
+
+  // broadcast to all clients (except incoming client) ----------------------------------
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    // socket.broadcast.emit("message", {
+    //   user: "disconnect",
+    //   content: `user: ${String(socket.id).slice(1, 5)} is disconnected --->`
+    // });
+  });
+  // ------------------------------------------------------------------------------------
 });
 
 // middleware error
